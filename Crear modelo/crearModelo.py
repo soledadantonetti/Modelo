a9d724a1ece8ee0b514814e685e00fd1a2f4b9b9
soledadantonetti <soledadantonetti@gmail.com>
2025-07-08 12:31:49 -0300

    Segundo coomiit prueba

--- Crear modelo/crearModelo.py
+++ Crear modelo/crearModelo.py
@@ -9,6 +9,8 @@ rutaBase = "C:/Proyectos Python/imagenes/casos/primerLote"
 nueva_ruta = "C:/Proyectos Python/imagenes/casos/paraProcesar/primerLote"
 
 
+#Recorre las carpetas de rutaBase y las imágenes que se muestran en cada carpeta las enúmero de forma secuencial y las guarda en nueva_ruta
+#Convierte las imágenes es JPG Y "RGB"
 
 def pasarAformatoCorrecto():
     contador = 0
@@ -39,22 +41,3 @@ def pasarAformatoCorrecto():
 
 
 pasarAformatoCorrecto()
-
-    
- #   for carpeta in carpetas:
-    #     imagenes = glob(os.path.join(carpeta, "*"))  # incluye .tif, .jpg, etc.
-
-    # for ruta in imagenes:
-    #     try:
-    #         img = Image.open(ruta)
-    #         if img.mode != "RGB":
-    #             # img = img.convert("RGB")
-    #             # nueva_ruta = os.path.splitext(ruta)[0] + ".jpg"
-    #             # img.save(nueva_ruta, format="JPEG")
-    #             # os.remove(ruta)  # opcional: eliminar el original .tif
-    #             # print(f"[OK] Convertida: {ruta} → {nueva_ruta}")
-    #         else:
-    #             print(f"[SKIP] Ya es RGB: {ruta}")
-    #     except Exception as e:
-    #         print(f"[ERROR] {ruta}: {e}")
-
